b1cef36d957bfb173a4074db148a5db6bf38fc6d
Teruya11 <daiki.teruya@usp.br>
2026-09-01 14:34:44 -0300

    Rename abstract model contract to BaseModel

--- src/spira_training/shared/core/models/trained_model.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class TrainedModel(ABC):
-    @abstractmethod
-    def dump_state(self) -> dict: ...
-
-    @abstractmethod
-    def load_state(self, state_dict: dict): ...
